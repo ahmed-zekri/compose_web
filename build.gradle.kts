8c0ae9f94cee6fe040fd1e22cba6f1c0b323f25f
E526729 <ahmed.zekri.a@gmail.com>
2022-09-20 01:29:10 +0100

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+// Add compose gradle plugin
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.0"
+}
+
+// Add maven repositories
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+// Enable JS(IR) target and add dependencies
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+// Fixes webpack-cli incompatibility by pinning the newest version.
+rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.10.0"
+}
